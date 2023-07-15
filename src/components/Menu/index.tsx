03fc73b6b4800fc1d0681f9b57c3470488c7f42e
Guilherme Sanches <guitdsanches@gmail.com>
2023-07-15 16:25:11 -0300

    feat: showing dropdown menu when clicking options

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,23 +5,38 @@ import { ReactComponent as Close } from 'assets/images/icon-close.svg'
 import classNames from 'classnames';
 import { useState } from 'react';
 const Menu = () => {
-    const [btnAtivo, setBtnAtivo] = useState(true);
-    const [contentActive, setContentActive] = useState(false);
+    const [btnAtivo, setBtnAtivo] = useState(true); /* hamburguer or close icon */
+    const [menuContentActive, setMenuContentActive] = useState(false); /* content when the menu is open */
+    const [dropdownActive, setDropdownActive] = useState({
+        product: false,
+        company: false,
+        connect: false
+    })
 
-    function handleClick() {
+    function handleClickIcon() {
         setBtnAtivo(!btnAtivo);
-        setContentActive(!contentActive);
+        setMenuContentActive(!menuContentActive);
     }
+
+    function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+        const value =
+            evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
+        setDropdownActive({
+            ...dropdownActive,
+            [evt.target.name]: value
+        });
+    }
+
     return (
         <nav>
             <Logo />
             <div className={styles.menu}>
                 <div className={styles.menu__icons}>
-                    <Hamburguer onClick={() => handleClick()} className={classNames({
+                    <Hamburguer onClick={() => handleClickIcon()} className={classNames({
                         [styles.menu__btn]: true,
                         [styles[`menu__btn-ativo`]]: btnAtivo
                     })} />
-                    <Close onClick={() => handleClick()} className={classNames({
+                    <Close onClick={() => handleClickIcon()} className={classNames({
                         [styles.menu__btn]: true,
                         [styles[`menu__btn-ativo`]]: !btnAtivo
                     })} />
@@ -29,12 +44,15 @@ const Menu = () => {
 
                 <div className={classNames({
                     [styles.menu__content]: true,
-                    [styles[`menu__content-active`]]: contentActive
+                    [styles[`menu__content-active`]]: menuContentActive
                 })}>
                     <ul>
                         <li className={styles.dropdown}>
-                            <a href="#" className={styles.dropdown__btn}>Product</a>
-                            <div className={styles.dropdown__content}>
+                            <label htmlFor="product">Product<input type='checkbox' id='product' name='product' onChange={(event) => handleChange(event)} /></label>
+                            <div className={classNames({
+                                [styles.dropdown__content]: true,
+                                [styles[`dropdown__content-active`]]: dropdownActive['product']
+                            })}>
                                 <a href="#">Overview</a>
                                 <a href="#">Princing</a>
                                 <a href="#">Marketplace</a>
@@ -43,8 +61,11 @@ const Menu = () => {
                             </div>
                         </li>
                         <li className={styles.dropdown}>
-                            <a href="#" className={styles.dropdown__btn}>Company</a>
-                            <div className={styles.dropdown__content}>
+                        <label htmlFor="company">Company<input type='checkbox' id='company' name='company' onChange={(event) => handleChange(event)} /></label>
+                            <div className={classNames({
+                                [styles.dropdown__content]: true,
+                                [styles[`dropdown__content-active`]]: dropdownActive['company']
+                            })}>
                                 <a href="#">About</a>
                                 <a href="#">Team</a>
                                 <a href="#">Blog</a>
@@ -52,8 +73,11 @@ const Menu = () => {
                             </div>
                         </li>
                         <li className={styles.dropdown}>
-                            <a href="#" className={styles.dropdown__btn}>Connect</a>
-                            <div className={styles.dropdown__content}>
+                        <label htmlFor="connect">Connect<input type='checkbox' id='connect' name='connect' onChange={(event) => handleChange(event)} /></label>
+                            <div className={classNames({
+                                [styles.dropdown__content]: true,
+                                [styles[`dropdown__content-active`]]: dropdownActive['connect']
+                            })}>
                                 <a href="#">Contact</a>
                                 <a href="#">Newsletter</a>
                                 <a href="#">LinkedIn</a>
